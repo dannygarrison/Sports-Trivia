b0bed57ce8235271aee45d1d8e8a842f1924f30b
dannygarrison <dannylgarrison@gmail.com>
2026-02-20 10:10:49 -0500

    Add WorldSeriesTrivia route to App component

--- App.jsx
+++ App.jsx
@@ -5,6 +5,7 @@ import NFLCollegeTrivia from './NFLCollegeTrivia.jsx'
 import SoccerLeaguesTrivia from './SoccerLeaguesTrivia.jsx'
 import NBAScorersGrid from './NBAScorersGrid.jsx'
 import SuperBowlTrivia from './SuperBowlTrivia.jsx'
+import WorldSeriesTrivia from './WorldSeriesTrivia.jsx'
 
 export default function App() {
   return (
@@ -16,6 +17,7 @@ export default function App() {
         <Route path="/games/soccer-leagues-trivia" element={<SoccerLeaguesTrivia />} />
         <Route path="/games/nba-scorers-grid" element={<NBAScorersGrid />} />
         <Route path="/games/super-bowl-history" element={<SuperBowlTrivia />} />
+        <Route path="/games/world-series-history" element={<WorldSeriesTrivia />} />
       </Routes>
     </div>
   )
