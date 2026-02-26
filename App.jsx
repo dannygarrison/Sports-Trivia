8b191c257120f5a2d02077326031c99fb8225a8c
dannygarrison <dannylgarrison@gmail.com>
2026-02-26 12:20:19 -0500

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,39 +1,39 @@
-import { lazy, Suspense } from ‘react’
-import { Routes, Route } from ‘react-router-dom’
-import Nav from ‘./Nav.jsx’
-import Home from ‘./Home.jsx’
+import { lazy, Suspense } from “react”
+import { Routes, Route } from “react-router-dom”
+import Nav from “./Nav.jsx”
+import Home from “./Home.jsx”
 
-const NFLCollegeTrivia = lazy(() => import(’./NFLCollegeTrivia.jsx’))
-const SoccerLeaguesTrivia = lazy(() => import(’./SoccerLeaguesTrivia.jsx’))
-const NBAScorersGrid = lazy(() => import(’./NBAScorersGrid.jsx’))
-const SuperBowlTrivia = lazy(() => import(’./SuperBowlTrivia.jsx’))
-const WorldSeriesTrivia = lazy(() => import(’./WorldSeriesTrivia.jsx’))
-const NFLNameDump = lazy(() => import(’./NFLNameDump.jsx’))
-const MLSTeams = lazy(() => import(’./MLSTeams.jsx’))
-const NFLChain = lazy(() => import(’./NFLChain.jsx’))
-const PLTeams = lazy(() => import(’./PLTeams.jsx’))
-const OlympicsHostCities = lazy(() => import(’./OlympicsHostCities.jsx’))
-const NFLMockDraft = lazy(() => import(’./NFLMockDraft.jsx’))
-const WhoHasMore = lazy(() => import(’./WhoHasMore.jsx’))
-const DraftClassQuiz = lazy(() => import(’./DraftClassQuiz.jsx’))
-const WhoWoreIt = lazy(() => import(’./WhoWoreIt.jsx’))
+const NFLCollegeTrivia = lazy(() => import(”./NFLCollegeTrivia.jsx”))
+const SoccerLeaguesTrivia = lazy(() => import(”./SoccerLeaguesTrivia.jsx”))
+const NBAScorersGrid = lazy(() => import(”./NBAScorersGrid.jsx”))
+const SuperBowlTrivia = lazy(() => import(”./SuperBowlTrivia.jsx”))
+const WorldSeriesTrivia = lazy(() => import(”./WorldSeriesTrivia.jsx”))
+const NFLNameDump = lazy(() => import(”./NFLNameDump.jsx”))
+const MLSTeams = lazy(() => import(”./MLSTeams.jsx”))
+const NFLChain = lazy(() => import(”./NFLChain.jsx”))
+const PLTeams = lazy(() => import(”./PLTeams.jsx”))
+const OlympicsHostCities = lazy(() => import(”./OlympicsHostCities.jsx”))
+const NFLMockDraft = lazy(() => import(”./NFLMockDraft.jsx”))
+const WhoHasMore = lazy(() => import(”./WhoHasMore.jsx”))
+const DraftClassQuiz = lazy(() => import(”./DraftClassQuiz.jsx”))
+const WhoWoreIt = lazy(() => import(”./WhoWoreIt.jsx”))
 
 export default function App() {
 return (
-<div style={{ minHeight: ‘100vh’, background: ‘#07070f’, color: ‘#e8e8f0’ }}>
+<div style={{ minHeight: “100vh”, background: “#07070f”, color: “#e8e8f0” }}>
 <Nav />
 <Suspense fallback={
 <div style={{
-minHeight: ‘100vh’,
-background: ‘#07070f’,
-display: ‘flex’,
-alignItems: ‘center’,
-justifyContent: ‘center’,
-color: ‘#ffffff40’,
+minHeight: “100vh”,
+background: “#07070f”,
+display: “flex”,
+alignItems: “center”,
+justifyContent: “center”,
+color: “#ffffff40”,
 fontFamily: “‘Oswald’, sans-serif”,
 fontSize: 13,
 letterSpacing: 3,
-textTransform: ‘uppercase’,
+textTransform: “uppercase”,
 }}>
 Loading…
 </div>
